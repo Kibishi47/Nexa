8fd43c85e7e388f5ca50e1c7f47b673302e5cea6
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2024-11-28 14:07:13 +0100

    Initial commit

---
+++ NexaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexaApp.swift
+//  Nexa
+//
+//  Created by Emmanuel Moulin on 28/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct NexaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
